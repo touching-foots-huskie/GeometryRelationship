c15fae69bc76d99701baf6bb4b84edadb989797a
Haonan Chang <chnme40cs@gmail.com>
2019-05-20 20:27:22 -0400

    update

--- GeometryRelationship/build/CMakeFiles/Makefile.cmake
+++ GeometryRelationship/build/CMakeFiles/Makefile.cmake
@@ -45,5 +45,5 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
-  "CMakeFiles/PoseEstimator.dir/DependInfo.cmake"
+  "CMakeFiles/GeometryRelationship.dir/DependInfo.cmake"
   )
--- GeometryRelationship/include/geometry_feature.hpp
+++ GeometryRelationship/include/geometry_feature.hpp
@@ -2,6 +2,7 @@
 #include <math.h>
 #include <Eigen/Core>
 #include <iostream>
+#define M_PI 3.141592653589793238462643383279502884
 
 namespace geometry_relation {
 
